9c050dcb88f34d2c0c6aac7d62c2289ec2f4652e
aondenet-sinval <sinvalsatv@gmail.com>
2023-05-17 21:00:34 -0300

    pass1

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
-
-let Person;
+let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+let Person;
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
